73c3ba012958fb1b3c96d22863f983fea0ffee1b
vulv04 <vulv04.it@gmail.com>
2025-07-01 15:33:50 +0700

    fix-js

--- src/common/middlewares/authMiddleware.js
+++ src/common/middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../../modules/user/user.model";
+import User from "../../modules/user/user.model.js";
 
 export const protect = async (req, res, next) => {
   let token;
